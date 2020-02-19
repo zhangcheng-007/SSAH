31ccddf9cd3814652a66add61c3cc77293f169e6
zhangcheng-007 <54843450+zhangcheng-007@users.noreply.github.com>
2020-02-19 19:03:29 +0800

    Update load_data.py

--- SSAH-master/load_data.py
+++ SSAH-master/load_data.py
@@ -8,8 +8,8 @@ def loading_data(path):
 
 	file = h5py.File(path)
 	images = file['images'][:].transpose(0, 3, 2, 1)
-	labels = file['LAll'][:].transpose(1, 0)
-	tags = file['YAll'][:].transpose(1, 0)
+	labels = file['LAll'][:].transpose()
+	tags = file['YAll'][:].transpose()
 	file.close()
 
 	return images, tags, labels
