b91cf28588d804e03e3f8e6a005c6a72ad8d7777
zhangcheng-007 <54843450+zhangcheng-007@users.noreply.github.com>
2020-02-20 15:45:06 +0800

    Update SSAH.py

--- SSAH-master/SSAH.py
+++ SSAH-master/SSAH.py
@@ -2,7 +2,7 @@ from setting import *
 from tnet import *
 import tensorflow as tf
 from ops import *
-from SSAH.SSAH-master.utils.calc_hammingranking import calc_map
+from utils.calc_hammingranking import calc_map
 import os
 import scipy.io as sio
 from tqdm import tqdm
